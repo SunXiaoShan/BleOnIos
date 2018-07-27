85d1d6e0d1bcf15783e0c26a23ea14d45a6a6cf4
Phineas Huang <Phineas.Huang@garmin.com>
2018-07-27 17:36:33 +0800

    Create project
    1. add central
    2. add peripheral

---
+++ Central/Central/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Central
+//
+//  Created by Phineas.Huang on 2018/7/27.
+//  Copyright © 2018 Phineas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Central/Central/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Central
+//
+//  Created by Phineas.Huang on 2018/7/27.
+//  Copyright © 2018 Phineas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
